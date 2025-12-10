a4b7e9b1a4cc9e3086084bb39765656771ceb4cd
Leonardo Nobrega <leonobr@fastmail.com>
2025-12-10 21:15:01 +0000

    test_local_timezone_setting

--- run_test.py
+++ run_test.py
@@ -1,3 +1,8 @@
 
-def test() -> None:
-    assert True
+import subprocess
+
+
+def test_local_timezone_setting() -> None:
+    completed_process = subprocess.run("date", capture_output=True)
+    date_output = completed_process.stdout.decode()
+    assert "EST" in date_output or "EDT" in date_output
