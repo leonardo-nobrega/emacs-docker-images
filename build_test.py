9529e86ec671869896d2bab8a2b6b792ffb58988
Leonardo Nobrega <leonobr@gmail.com>
2025-12-19 13:50:05 -0500

    test_tree_sitter_grammars_exist

--- build_test.py
+++ build_test.py
@@ -1,7 +1,10 @@
 
 import io
+import json
+import re
 
 from pathlib import Path
+from typing import Callable
 
 
 def test_man_pages_exist() -> None:
@@ -24,6 +27,29 @@ def snip_block(stream: io.TextIOBase, start: str, end: str) -> list[str]:
     return result
 
 
+def find_missing_items(
+        required: list[str],
+        existing: list[str],
+        matches: Callable[[str, str], bool]
+) -> list[str]:
+    result = []
+    required.sort()
+    existing.sort()
+    for item_required in required:
+        found = False
+        for item_existing in existing:
+            if matches(item_existing, item_required):
+                found = True
+                break
+        if not found:
+            result.append(item_required)
+
+    log_dict = {"required": required, "existing": existing, "result": result}
+    print("find_missing_items", json.dumps(log_dict, indent=2))
+
+    return result
+
+
 def test_emacs_packages_exist() -> None:
     with open("install.el") as f:
         package_symbols = snip_block(
@@ -37,14 +63,50 @@ def test_emacs_packages_exist() -> None:
         if p.is_dir()
     ]
 
-    missing_packages = []
-    for package in packages:
-        found = False
-        for name in names_of_directories_in_emacs_d:
-            if name.startswith(package):
-                found = True
-                break
-        if not found:
-            missing_packages.append(package)
+    def directory_starts_with_package(directory: str, package: str) -> bool:
+        return directory.startswith(package)
+
+    missing_packages = find_missing_items(
+        packages,
+        names_of_directories_in_emacs_d,
+        directory_starts_with_package
+    )
 
     assert not missing_packages
+
+
+def test_tree_sitter_grammars_exist() -> None:
+    # a token is a char sequence without parentheses or spaces
+    TOKEN_REGEXP = re.compile("[^'() ]+")
+
+    def tree_sitter_grammar(line: str) -> str:
+        # the first token in each item of treesit-language-source-alist
+        # is the name of the grammar
+        tokens = TOKEN_REGEXP.findall(line)
+        return tokens[0]
+
+    with open("install.el") as f:
+        tree_sitter_lines = snip_block(
+            f, ";; start-tree-sitter-list", ";; end-tree-sitter-list"
+        )
+
+    tree_sitter_grammars = [
+        tree_sitter_grammar(line) for line in tree_sitter_lines
+    ]
+
+    names_of_libraries = [
+        p.name
+        for p in Path("/home/docker_usr/.emacs.d/tree-sitter").glob("**")
+        if p.is_file()
+    ]
+
+    def library_name_contains_grammar(library_name: str, grammar: str) -> bool:
+        return grammar in library_name
+
+    missing_libraries = find_missing_items(
+        tree_sitter_grammars,
+        names_of_libraries,
+        library_name_contains_grammar
+    )
+
+    assert not missing_libraries
